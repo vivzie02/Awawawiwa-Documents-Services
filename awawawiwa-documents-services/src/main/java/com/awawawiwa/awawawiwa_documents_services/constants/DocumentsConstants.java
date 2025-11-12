9644bcb8df7f40f3d44116f0d8abad06ce8ae5dc
vivzie02 <101602263+vivzie02@users.noreply.github.com>
2025-11-12 19:12:23 +0100

    AW-37 | correct confirmationUrl

--- awawawiwa-documents-services/src/main/java/com/awawawiwa/awawawiwa_documents_services/constants/DocumentsConstants.java
+++ awawawiwa-documents-services/src/main/java/com/awawawiwa/awawawiwa_documents_services/constants/DocumentsConstants.java
@@ -1,6 +1,6 @@
 package com.awawawiwa.awawawiwa_documents_services.constants;
 
 public final class DocumentsConstants {
-    public static String confirmationEmailUrl = "http://localhost:8080/api/auth/confirm?token=";
+    public static String confirmationEmailUrl = "http://localhost:50352/api/auth/confirm?token=";
     public static String confirmationEmailTemplate = "confirmation-email.html";
 }
